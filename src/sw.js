e74b7c27d2e38a0dff209e78ed99562125377355
Brian Zalewski <brian@megabyte.space>
2023-03-15 22:41:19 -0400

    Latest

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
 importScripts('workbox-v6.5.4/workbox-sw.js');
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
 
-self.workbox.precaching.precacheAndRoute([]);
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
